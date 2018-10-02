529329be9e2b411ee60eb3831801484b1a4fdee5
yui-knk <spiketeika@gmail.com>
2018-10-02 20:55:30 +0900

    Add eval test cases

--- src/eval.rs
+++ src/eval.rs
@@ -34,3 +34,31 @@ impl Evaluator {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use value::{Value};
+    use parser::{Parser};
+
+    fn eval_string(str: String) -> Result<Value, Error> {
+        let mut parser = Parser::new(str);
+        let noed = parser.parse().unwrap();
+        let eval = Evaluator::new();
+        eval.eval(&noed)
+    }
+
+    #[test]
+    fn test_eval_false() {
+        let result = eval_string("false".to_string());
+
+        assert_eq!(result, Ok(Value::new_false()));
+    }
+
+    #[test]
+    fn test_eval_true() {
+        let result = eval_string("true".to_string());
+
+        assert_eq!(result, Ok(Value::new_true()));
+    }
+}
